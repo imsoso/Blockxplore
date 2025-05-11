18d382645aa4002f53da4a0777920c478cac5c39
Soso <husosong@163.com>
2025-05-11 15:00:44 +0800

    feat: init project

---
+++ BlockXplore/BlockXplore/BlockXploreApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockXploreApp.swift
+//  BlockXplore
+//
+//  Created by 1234 on 2025/5/11.
+//
+
+import SwiftUI
+
+@main
+struct BlockXploreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
