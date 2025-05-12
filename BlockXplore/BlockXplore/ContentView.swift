d838fd231c23d2b27a4f5e7105f57a699239eea6
Soso <husosong@163.com>
2025-05-12 11:04:38 +0800

    feat: get account info by address

--- BlockXplore/BlockXplore/ContentView.swift
+++ BlockXplore/BlockXplore/ContentView.swift
@@ -6,23 +6,29 @@
 //
 
 import SwiftUI
-import SolanaSwift
+import Solana
+
 
 struct ContentView: View {
     @State private var address: String = ""
     @State private var selectedNetwork: String = "Mainnet" // Local variable to store the selected network
 
-    var endpoint: APIEndPoint {
-        let ApiAddress = "https://devnet.helius-rpc.com/?api-key=7ad72601-5330-4af0-b238-175e3ed057d1"
-        switch selectedNetwork {
-        case "Testnet":
-            return APIEndPoint(address: ApiAddress, network: .testnet)
-        case "Devnet":
-            return APIEndPoint(address: ApiAddress, network: .devnet)
-        default:
-            return APIEndPoint(address: ApiAddress, network: .mainnetBeta)
-        }
+    
+    public static let mainnetBetaAnkr = RPCEndpoint(
+        url: URL(string: "https://mainnet.helius-rpc.com/?api-key=7d9d37dd-ad9b-499c-98f72")!,
+        urlWebSocket: URL(string: "https://mainnet.helius-rpc.com/?api-key=7d9d37dd-ad9b-499c-98f7")!,
+        network: .mainnetBeta
+    )
+    
+    let endpoint = mainnetBetaAnkr
+    let router: NetworkingRouter
+    let solana: Solana
+
+    init() {
+        self.router = NetworkingRouter(endpoint: endpoint)
+        self.solana = Solana(router: router)
     }
+
 
     var body: some View {
         NavigationView {
@@ -38,7 +44,7 @@ struct ContentView: View {
                 
                 Button(action: {
                     Task {
-                        await checkAddress()
+                        await fetchAccountInfo(for: address)
                     }
                 }) {
                     Label("Check", systemImage: "magnifyingglass")
@@ -61,13 +67,26 @@ struct ContentView: View {
         }
     }
     
-    func checkAddress() async {
-        do {
-            let apiClient = JSONRPCAPIClient(endpoint: endpoint)
-            let result = try await apiClient.getBlockHeight()
-            print("Block height: \(result)")
-        } catch {
-            print("Error fetching block height: \(error)")
+    
+    func fetchAccountInfo(for address: String) async {
+        print("address: \(address)")
+        var retryCount = 0
+        let maxRetries = 3
+
+        while retryCount < maxRetries {
+            do {
+                let info: BufferInfo<AccountInfo> = try await solana.api.getAccountInfo(account: address, decodedTo: AccountInfo.self)
+                print("Account Info: \(info)")
+                return
+            } catch {
+                retryCount += 1
+                print("Error fetching account info (attempt \(retryCount)): \(error)")
+                if retryCount == maxRetries {
+                    print("Max retry attempts reached. Please check your network connection.")
+                } else {
+                    await Task.sleep(2 * 1_000_000_000) // Wait for 2 seconds before retrying
+                }
+            }
         }
     }
 }
